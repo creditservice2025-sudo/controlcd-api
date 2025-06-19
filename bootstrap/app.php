0ac00ad4ff3fa2afe4f03be8f419406535ba7f52
Marcos Hernandez <maalhequi95@gmail.com>
2025-06-19 09:08:54 -0500

    enable corst on bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
+        
+         $middleware->validateCsrfTokens(except: [
+			'*',
+		]);
 
         //
     })
