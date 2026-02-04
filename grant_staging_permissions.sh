694af4e6337f7c83779dfcfd5b3bf40478e376ae
Mario Jose Diaz Amariscua <mjdamariscua@gmail.com>
2026-02-04 18:00:03 -0500

    chore: Update `DB_NAME` in `grant_staging_permissions.sh` to reflect the new database name.

--- grant_staging_permissions.sh
+++ grant_staging_permissions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DB_NAME="control-cd-20260203"
+DB_NAME="control-cd-20260204"
 SSH_KEY="/home/mario-d-az/.ssh/id_rsa_mario_controlcd"
 REMOTE_HOST="root@146.190.147.164"
 USER="staging-controlcd"
