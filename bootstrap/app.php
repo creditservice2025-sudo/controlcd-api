6b3871832f782eb481d4a148f33d408d42028510
Mario Jose Diaz Amariscua <mjdamariscua@gmail.com>
2026-02-04 20:47:42 -0500

    config: disable CSRF token validation for all routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,7 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->validateCsrfTokens(except: [
-            //
+            '*'
         ]);
 
         //
